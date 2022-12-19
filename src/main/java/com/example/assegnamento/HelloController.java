d3aa2dbf9eaed2e112fe06b1346fcb82c5ec3560
elia2904 <candidaelia29@gmail.com>
2022-12-19 19:53:36 +0100

    aggiunta funzione al pulsante Registra Clienti (ex registrati) che apre pop-up di registrazione

--- src/main/java/com/example/assegnamento/HelloController.java
+++ src/main/java/com/example/assegnamento/HelloController.java
@@ -3,12 +3,19 @@ package com.example.assegnamento;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 //import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.*;
 
-//import com.example.assegnamento.DBHelper;
+//import com.example.assegnamento.DBHelper; ??????? ci va o no?
 
 public class HelloController {
 
@@ -23,6 +30,24 @@ public class HelloController {
 
     @FXML
     private TextField username;
+
+    @FXML
+    void OnClickOpenRegister(ActionEvent event) {
+
+
+        try {
+            Parent root = FXMLLoader.load(HelloApplication.class.getResource("register.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Registrazione Cliente");
+            stage.setScene(new Scene(root));
+            //blocca finestra prima
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     void onLoginButtonClick(ActionEvent event) throws SQLException {
