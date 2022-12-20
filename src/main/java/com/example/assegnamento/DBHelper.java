f05c5a4d2c45cfaee3aeb95a08d0277a84783c2d
elia2904 <candidaelia29@gmail.com>
2022-12-20 01:38:57 +0100

    aggiunta funzione per poter aggiungere nuovi record al DB (update)/ modificare forse, non ne sono sicuro. Refactoring minore

--- src/main/java/com/example/assegnamento/DBHelper.java
+++ src/main/java/com/example/assegnamento/DBHelper.java
@@ -3,24 +3,34 @@ import java.sql.*;
 
 public class DBHelper {
 
-    public static ResultSet query(String q)// mettere ResultSet invece di int
+    public static ResultSet query(String sql)  //produce risultati di una query
     {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wineshop","root","");
 
             Statement statement = con.createStatement();
 
-            ResultSet resultSet = statement.executeQuery(q);
-
-
-            return resultSet;
+            return statement.executeQuery(sql);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public static void update(String sql)    //executeUpdate per aggiungere record o modificarli(?)
+    {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wineshop","root","");
 
+            Statement statement = con.createStatement();
+
+            statement.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 
 }
