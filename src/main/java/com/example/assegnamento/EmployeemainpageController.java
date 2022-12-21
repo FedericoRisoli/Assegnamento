199f160c666e44bfe60e297e37041c395bd2dff8
Lyfaen666 <fede8906@gmail.com>
2022-12-21 11:38:35 +0100

    Creato modifica password sia controller che fxml per permettere ad admin e utenti di modificare la loro password (query da implementare)

--- src/main/java/com/example/assegnamento/EmployeemainpageController.java
+++ src/main/java/com/example/assegnamento/EmployeemainpageController.java
@@ -2,14 +2,21 @@ package com.example.assegnamento;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EmployeemainpageController {
 
     @FXML
-    private ComboBox<?> annata;
+    private ComboBox<String> annata;
 
     @FXML
     private TextField nome_vino;
@@ -23,7 +30,13 @@ public class EmployeemainpageController {
     }
 
     @FXML
-    void OnButtonModifyPSWClick(ActionEvent event) {
+    void OnButtonModifyPSWClick(ActionEvent event) throws IOException {
+        //creare pop up per modificare psw
+       /* Parent root = FXMLLoader.load(HelloApplication.class.getResource("modifypsw.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Modifica Password");
+        stage.setScene(new Scene(root));
+        stage.show();*/
 
     }
 
---
+++ src/main/java/com/example/assegnamento/ModifypswController.java
@@ -0,0 +1,28 @@
+package com.example.assegnamento;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class ModifypswController {
+
+    @FXML
+    private PasswordField confirmpassword;
+
+    @FXML
+    private PasswordField newpassword;
+
+    @FXML
+    private PasswordField oldpassword;
+
+    @FXML
+    void OnCambiaPasswordButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnindietrobuttonClick(ActionEvent event) {
+
+    }
+
+}
