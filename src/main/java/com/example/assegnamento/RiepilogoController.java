2b2520789ec43339f75bcb484213f271b926faf6
elia2904 <candidaelia29@gmail.com>
2022-12-28 21:07:44 +0100

    fixato bug ui ui non completa

--- src/main/java/com/example/assegnamento/RiepilogoController.java
+++ src/main/java/com/example/assegnamento/RiepilogoController.java
@@ -1,4 +1,11 @@
 package com.example.assegnamento;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
 public class RiepilogoController {
+
+    @FXML
+    private ToggleGroup metodo;
+
 }
