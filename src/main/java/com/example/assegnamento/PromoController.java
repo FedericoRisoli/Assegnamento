f9046b76bd1c1778d6e78729fe619130ca8dd0a0
Lyfaen666 <fede8906@gmail.com>
2023-01-01 21:13:23 +0100

    PRomo completata

--- src/main/java/com/example/assegnamento/PromoController.java
+++ src/main/java/com/example/assegnamento/PromoController.java
@@ -2,6 +2,7 @@ package com.example.assegnamento;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -14,12 +15,13 @@ public class PromoController {
 
 
     @FXML
+    private TableView<Vini> tabella;
+    @FXML
     private TableColumn<Vini, String> t_anno;
     @FXML
     private TableColumn<Vini, String> t_nome;
     @FXML
     private TableColumn<Vini, String> t_note;
-
     @FXML
     private TableColumn<Vini, Double> t_prezzo;
     @FXML
@@ -31,27 +33,29 @@ public class PromoController {
     private TableColumn<Vini, String> t_vitigno;
     @FXML
     private TableColumn<Vini, String> t_provenienza;
-    @FXML
-    private TableView<Vini> tabella;
     public Data data =Data.getInstance();
 
     public void initialize() throws SQLException {
+        ResultSet r;
         data.SetPromo(false);
-        ResultSet  r = DBHelper.query("SELECT * FROM `wines` WHERE 'promo' LIKE '1' ORDER BY `promo` DESC");
         t_nome.setCellValueFactory(new PropertyValueFactory<Vini, String>("Nome"));
         t_produttore.setCellValueFactory(new PropertyValueFactory<Vini, String>("p1"));
         t_provenienza.setCellValueFactory(new PropertyValueFactory<Vini, String>("p2"));
         t_anno.setCellValueFactory(new PropertyValueFactory<Vini, String>("a"));
         t_vitigno.setCellValueFactory(new PropertyValueFactory<Vini, String>("v"));
         t_note.setCellValueFactory(new PropertyValueFactory<Vini, String>("not"));
+
         t_qta.setCellValueFactory(new PropertyValueFactory<Vini, Spinner<Integer>>("spin"));
         t_prezzo.setCellValueFactory(new PropertyValueFactory<Vini, Double>("prezzo"));
 
         ObservableList<Vini> tmp = FXCollections.observableArrayList();
 
 
-        while (r.next()) {
-            tmp.add(new Vini(r.getString("nome"), r.getString("produttore"), r.getString("provenienza"), r.getString("anno"), r.getString("vitigno"), r.getString("notetecniche"), r.getString("qualita"), r.getString("vendite"), r.getString("promo")));
+        //popolo ListView
+        r = DBHelper.query("SELECT * FROM `wines` WHERE promo LIKE '1'");
+        while(r.next())
+        {
+            tmp.add(new Vini(r.getString("nome"),r.getString("produttore"),r.getString("provenienza"), r.getString("anno"), r.getString("vitigno"), r.getString("notetecniche"),r.getString("qualita"), r.getString("vendite"), r.getString("promo")));
             tabella.setItems(tmp);
         }
     }
