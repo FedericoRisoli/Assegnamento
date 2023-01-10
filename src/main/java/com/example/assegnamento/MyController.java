d317a06c6d9e64ce55236dd203544e7153efe441
elia2904 <118290661+elia2904@users.noreply.github.com>
2023-01-10 02:46:10 +0100

    snellito file

--- src/main/java/com/example/assegnamento/MyController.java
+++ src/main/java/com/example/assegnamento/MyController.java
@@ -1,7 +1,6 @@
 package com.example.assegnamento;
 
 public class MyController {
-    ServerConnection canale =ServerConnection.getInstance();
 
     public void handleMessage(String message) {
         System.out.println("Ho ricevuto questo messaggio, ma non so che farne, usa override per implementare un metodo adeguato.\n Messaggio: "+message);
