88f616205e3c2ab39eb189e17af9ce65e8777f29
Lyfaen666 <fede8906@gmail.com>
2023-01-10 18:00:15 +0100

    Creato controller per proposta acquisto

---
+++ src/main/java/com/example/assegnamento/PropostaacquistoController.java
@@ -0,0 +1,33 @@
+package com.example.assegnamento;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PropostaacquistoController
+{
+
+        @FXML
+        private Button OnButtonAnnullaClick;
+
+        @FXML
+        private Button OnButtonInviaClick;
+
+        @FXML
+        private TableColumn<?, ?> t_propname;
+
+        @FXML
+        private TableColumn<?, ?> t_propqta;
+
+        @FXML
+        private TableColumn<?, ?> t_propsel;
+
+        @FXML
+        private TableView<?> tabellapropac;
+
+    }
+
+
