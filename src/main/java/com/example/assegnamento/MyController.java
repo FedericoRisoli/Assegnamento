01980b29ecbf914d7151b1587126408d9f14939c
elia2904 <118290661+elia2904@users.noreply.github.com>
2023-01-12 18:11:05 +0100

    Aggiunti/cambiati diversi metodi cambiato handleMessage aggiunti sendMessage, killChildThread, changeScene minor changes

--- src/main/java/com/example/assegnamento/MyController.java
+++ src/main/java/com/example/assegnamento/MyController.java
@@ -1,8 +1,61 @@
 package com.example.assegnamento;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class MyController {
 
+    ServerConnection server=ServerConnection.getInstance();
+
+    //inizializzazione nel controller figlio con createTask
+    MessageReceiverTask task;
+
+    Stage stage;
+    Scene scene;
+    Parent root;
+
     public void handleMessage(String message) {
-        System.out.println("Ho ricevuto questo messaggio, ma non so che farne, usa override per implementare un metodo adeguato.\n Messaggio: "+message);
+        if (message.equals("Chiusura Thread Figlio, arrivederci"))
+            System.out.println(message);
+        else
+            System.out.println("------\nERRORE:\nhandleMessage() non implementato\n guarda README\n Messaggio ricevuto:\n"+message+"\n------");
     }
+
+    //da cancellare
+    public void createTask(Socket socket, MyController controller){
+        this.task=new MessageReceiverTask(socket, controller);
+        new Thread(task).start();
+    }
+
+    public void sendMessage(String message){
+        server.sendMessage(message);
+    }
+
+    public void killChildThread(){
+        task.stop();
+        server.sendMessage("Chiusura Thread Figlio, arrivederci");
+    }
+
+    public void changeScene(String NomeScenaFXML, String NuovoTitolo, ActionEvent event){
+        killChildThread();
+        try {
+
+            root = FXMLLoader.load(getClass().getResource(NomeScenaFXML));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
