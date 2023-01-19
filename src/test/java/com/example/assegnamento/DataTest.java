6d66d806c3aa9332131ac531c826168eac21c70e
elia2904 <118290661+elia2904@users.noreply.github.com>
2023-01-19 17:29:33 +0100

    Update DataTest.java

--- src/test/java/com/example/assegnamento/DataTest.java
+++ src/test/java/com/example/assegnamento/DataTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestData {
+class DataTest {
     @Test
     public void TestGetInstance() {
         Data data = Data.getInstance();
