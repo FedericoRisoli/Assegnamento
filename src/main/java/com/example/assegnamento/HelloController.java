43f4a7f0fa5ba5ee63e37bfbd35c1ae8082772f5
Elia <candidaelia29@gmail.com>
2023-03-09 14:58:53 +0100

    rimosso import non utilizzato

--- src/main/java/com/example/assegnamento/HelloController.java
+++ src/main/java/com/example/assegnamento/HelloController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
